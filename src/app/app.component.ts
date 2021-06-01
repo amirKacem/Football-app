bd29aa9b3dd14c194470d352f0a6e4d431a5018b
amirkacem <amir.kassem.ben.ammou@gmail.com>
2021-06-01 17:05:10 +0100

    Storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { Storage } from '@ionic/storage-angular';
+import {NavController} from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(private storage: Storage,private navCtrl:NavController) {
+    
+    this.initalizae
+  }
+  async ngOnInit() {
+    
+    await this.storage.create();
+  }
+  
+ 
+  initalizae(){
+    this.storage.get('session').then((res) => {
+      console.log(res);
+       this.navCtrl.navigateRoot('/login');
+       if(res==null){
+      }else{
+        this.navCtrl.navigateRoot('/home');
+      }
+    });
+  }
+
 }
